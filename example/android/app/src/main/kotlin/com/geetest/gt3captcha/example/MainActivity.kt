9ac832b8f7cfcf204b8182d08db3e6a9b5f04914
NikoXu <chaosvita@icloud.com>
2021-10-26 12:00:32 +0800

    Create example for ios

---
+++ example/android/app/src/main/kotlin/com/geetest/gt3captcha/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.geetest.gt3captcha.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert' as convert;
@@ -35,47 +37,85 @@ class _MyAppState extends State<MyApp> {
     try {
       platformVersion =
           await Gt3FlutterPlugin.platformVersion ?? 'Unknown platform version';
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    try {
       captcha.addEventHandler(
         onShow: (Map<String, dynamic> message) async {
-          print("Captcha did show");
+          // TO-DO
+          // 验证视图已展示
+          debugPrint("Captcha did show");
+        },
+        onClose: (Map<String, dynamic> message) async {
+          // TO-DO
+          // 验证视图已关闭
+          debugPrint("Captcha did close");
         },
         onResult: (Map<String, dynamic> message) async {
-          print("Captcha result: " + message.toString());
+          debugPrint("Captcha result: " + message.toString());
           String code = message["code"];
           if (code == "1") {
             // TO-DO
             // 发送 message["result"] 中的数据向 B 端的业务服务接口进行查询
             // 对结果进行二次校验
+            debugPrint("Captcha result code : " + code);
           }
-          else if (code == "0") {
+          else {
             // 终端用户完成验证失败，自动重试
+            debugPrint("Captcha result code : " + code);
           }
         },
         onError: (Map<String, dynamic> message) async {
-          print("Captcha error: " + message.toString());
+          debugPrint("Captcha error: " + message.toString());
           String code = message["code"];
+
+          // TO-DO
           //处理验证中返回的错误
-          if (code == "-1001") {
-            // 网络超时
+          if (Platform.isAndroid) { // Android 平台
+
           }
-          else if (code == "-999") {
-            // 请求被意外中断, 一般由用户进行取消操作导致
+
+          if (Platform.isIOS) { // iOS 平台
+            if (code == "-1009") {
+              // 网络无法访问
+            }
+            else if (code == "-1004") {
+              // 无法查找到 HOST 
+            }
+            else if (code == "-1002") {
+              // 非法的 URL
+            }
+            else if (code == "-1001") {
+              // 网络超时
+            }
+            else if (code == "-999") {
+              // 请求被意外中断, 一般由用户进行取消操作导致
+            }
+            else if (code == "-21") {
+              // 使用了重复的 challenge
+              // 检查获取 challenge 是否进行了缓存
+            }
+            else if (code == "-20") {
+              // 尝试过多, 重新引导用户触发验证即可
+            }
+            else if (code == "-10") {
+              // 预判断时被封禁, 不会再进行图形验证
+            }
+            else if (code == "-1") {
+              // Gt3RegisterData 参数不合法
+            }
+            else {
+              // 更多错误码参考开发文档
+              // https://docs.geetest.com/sensebot/apirefer/errorcode/ios
+            }
           }
-          else if (code == "-10") {
-            // 预判断时被封禁, 不会再进行图形验证
-          }
-          else if (code == "-20") {
-            // 尝试过多, 重新引导用户触发验证即可
-          }
-          else if (code == "-1") {
-            // 参数不合法
-          }
-          else {
-            // 网络问题或解析失败, 更多错误码参考开发文档
-          }
+          
         });
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+    }
+    catch (e) {
+      debugPrint("Event handler exception " + e.toString());
     }
 
     // If the widget was removed from the tree while the asynchronous platform
@@ -88,23 +128,28 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  Future<dynamic> startCaptcha() async {
-    try {
-      final response = await http.get(Uri.parse("https://www.geetest.com/demo/gt/register-click?t=\(DateTime.now().millisecondsSinceEpoch)"));
+  // 开始
+  void startCaptcha() async {
+    debugPrint("Start captcha. Current version: " + _platformVersion);
+    // 添加时间戳，避免缓存
+      // 如果 challenge 缓存，重复使用，会收到 -21 等错误
+      String url = "https://www.geetest.com/demo/gt/register-click?t=" + DateTime.now().toString();
+      final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
         var jsonResponse =
         convert.jsonDecode(response.body) as Map<String, dynamic>;
         Gt3RegisterData registerData = Gt3RegisterData(
           gt: jsonResponse["gt"],
           challenge: jsonResponse["challenge"],
-          success: jsonResponse["success"]);
-      return await captcha.startCaptcha(registerData);
+          success: jsonResponse["success"] == 1);
+        captcha.startCaptcha(registerData);
       }
-    } on PlatformException catch (e) {
-      print('PlatformException');
-      return '-1';
-    }
-}
+  }
+
+  // 关闭
+  void close() {
+    captcha.close();
+  }
 
   @override
   Widget build(BuildContext context) {
