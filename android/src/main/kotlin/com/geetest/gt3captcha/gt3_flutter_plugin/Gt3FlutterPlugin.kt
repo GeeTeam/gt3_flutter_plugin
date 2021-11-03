3253870eb45458efb413bbeae18da63cd6fe8e8c
chenruixiao <chenruixiao@geetest.com>
2021-11-03 16:55:27 +0800

    [feature/0.0.2] Initial version of Android plugin

--- android/src/main/kotlin/com/geetest/gt3captcha/gt3_flutter_plugin/Gt3FlutterPlugin.kt
+++ android/src/main/kotlin/com/geetest/gt3captcha/gt3_flutter_plugin/Gt3FlutterPlugin.kt
@@ -1,35 +1,139 @@
 package com.geetest.gt3captcha.gt3_flutter_plugin
 
+import android.app.Activity
+import android.os.Build
+import android.util.Log
 import androidx.annotation.NonNull
-
+import com.geetest.sdk.GT3ConfigBean
+import com.geetest.sdk.GT3ErrorBean
+import com.geetest.sdk.GT3GeetestUtils
+import com.geetest.sdk.GT3Listener
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import org.json.JSONObject
 
-/** Gt3FlutterPlugin */
-class Gt3FlutterPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class Gt3FlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "gt3_flutter_plugin")
-    channel.setMethodCallHandler(this)
-  }
+    private lateinit var gt3GeetestUtils: GT3GeetestUtils
+    private lateinit var gt3ConfigBean: GT3ConfigBean
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    private var activity: Activity? = null
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "gt3_flutter_plugin")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
+            "startCaptcha" -> {
+                startCaptchaInner(call, result)
+            }
+            "getPlatformVersion" -> {
+                result.success("Android ${Build.VERSION.RELEASE}")
+            }
+            else -> {
+                result.notImplemented()
+            }
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        this.activity = binding.activity
+        geetestInit()
+    }
+
+    override fun onDetachedFromActivity() {
+        gt3GeetestUtils.destory()
+        this.activity = null
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        gt3GeetestUtils.destory()
+        this.activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        onAttachedToActivity(binding)
+    }
+
+    private fun startCaptchaInner(call: MethodCall, result: Result) {
+        val args: Map<String, Any> = call.arguments()
+        Log.i(TAG, "Geetest captcha register params: $args")
+
+        if (args.containsKey("gt") &&
+            args.containsKey("challenge") &&
+            args.containsKey("success")
+        ) {
+            gt3GeetestUtils.startCustomFlow()
+            val geetestId = args["gt"]
+            val geetestChallenge = args["challenge"]
+            val geetestSuccess = if (args["success"] == null) 0 else 1
+            gt3ConfigBean.api1Json =
+                JSONObject("{\"success\":$geetestSuccess,\"challenge\":\"$geetestChallenge\",\"gt\":\"$geetestId\",\"new_captcha\":true}")
+            gt3GeetestUtils.getGeetest()
+        } else {
+            val ret: Map<String, String> = mapOf(
+                "initWithDomain" to "com.geetest.gt3.flutter",
+                "code" to "-1",
+                "userInfo" to "Register params parse invalid"
+            )
+            channel.invokeMethod("onError", ret)
+        }
+    }
+
+    private fun geetestInit() {
+        gt3GeetestUtils = GT3GeetestUtils(activity)
+        gt3ConfigBean = GT3ConfigBean()
+        gt3ConfigBean.apply {
+            pattern = 1
+            listener = object : GT3Listener() {
+                override fun onDialogReady(duration: String?) {
+                    channel.invokeMethod("onShow", mapOf("show" to "1"))
+                }
+
+                override fun onDialogResult(result: String?) {
+                    gt3GeetestUtils.showSuccessDialog()
+                }
+
+                override fun onReceiveCaptchaCode(p0: Int) {
+                    Log.i(TAG, "Geetest captcha code: $p0")
+                    channel.invokeMethod("onResult", mapOf("code" to "$p0"))
+                }
+
+                override fun onStatistics(p0: String?) {}
+
+                override fun onClosed(p0: Int) {
+                    channel.invokeMethod("onClose", mapOf("close" to "$p0"))
+                }
+
+                override fun onSuccess(p0: String?) {}
+
+                override fun onFailed(p0: GT3ErrorBean?) {
+                    val ret = mapOf(
+                        "code" to p0?.errorCode,
+                        "description" to p0?.errorDesc
+                    )
+                    channel.invokeMethod("onError", ret)
+                }
+
+                override fun onButtonClick() {}
+            }
+        }
+        gt3GeetestUtils.init(gt3ConfigBean)
+    }
+
+    private companion object {
+        const val TAG = "Gt3FlutterPlugin"
+    }
 }
