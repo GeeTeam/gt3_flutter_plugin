9e0df5e7a3e491efec6c6af5e43c91915a738fec
chenruixiao <chenruixiao@geetest.com>
2021-11-04 12:28:34 +0800

    [feature/0.0.2] Fix lint issues

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -210,17 +210,19 @@ class _MyAppState extends State<MyApp> {
                 foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
                 overlayColor: MaterialStateProperty.resolveWith<Color?>(
                   (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.hovered))
+                    if (states.contains(MaterialState.hovered)) {
                       return Colors.blue.withOpacity(0.04);
+                    }
                     if (states.contains(MaterialState.focused) ||
-                        states.contains(MaterialState.pressed))
+                        states.contains(MaterialState.pressed)) {
                       return Colors.blue.withOpacity(0.12);
+                    }
                     return null; // Defer to the widget's default.
                   },
                 ),
               ),
               onPressed: startCaptcha,
-              child: Text('点击验证')),
+              child: const Text('点击验证')),
         ),
       ),
     );
