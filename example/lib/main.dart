27f1b576e1054bd22f7cc0500542d7c4407a17d4
chenruixiao <chenruixiao@geetest.com>
2021-11-04 11:48:58 +0800

    [feature/0.0.2] Run a pass of flutter format

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -42,112 +42,90 @@ class _MyAppState extends State<MyApp> {
     }
 
     try {
-      captcha.addEventHandler(
-        onShow: (Map<String, dynamic> message) async {
+      captcha.addEventHandler(onShow: (Map<String, dynamic> message) async {
+        // TO-DO
+        // 验证视图已展示
+        debugPrint("Captcha did show");
+      }, onClose: (Map<String, dynamic> message) async {
+        // TO-DO
+        // 验证视图已关闭
+        debugPrint("Captcha did close");
+      }, onResult: (Map<String, dynamic> message) async {
+        debugPrint("Captcha result: " + message.toString());
+        String code = message["code"];
+        if (code == "1") {
           // TO-DO
-          // 验证视图已展示
-          debugPrint("Captcha did show");
-        },
-        onClose: (Map<String, dynamic> message) async {
-          // TO-DO
-          // 验证视图已关闭
-          debugPrint("Captcha did close");
-        },
-        onResult: (Map<String, dynamic> message) async {
-          debugPrint("Captcha result: " + message.toString());
-          String code = message["code"];
-          if (code == "1") {
-            // TO-DO
-            // 发送 message["result"] 中的数据向 B 端的业务服务接口进行查询
-            // 对结果进行二次校验
-            var result = message["result"] as Map;
-            debugPrint("Captcha result: " + result.toString());
-            // debugPrint("Captcha result code: " + code + ", result: " + jsonResult.toString());
-            await validateCaptchaResult(result.map((key, value) => MapEntry(key.toString(), value.toString())));
-          }
-          else {
-            // 终端用户完成验证失败，自动重试
-            debugPrint("Captcha result code: " + code);
-          }
-        },
-        onError: (Map<String, dynamic> message) async {
-          debugPrint("Captcha error: " + message.toString());
-          String code = message["code"];
+          // 发送 message["result"] 中的数据向 B 端的业务服务接口进行查询
+          // 对结果进行二次校验
+          var result = message["result"] as Map;
+          debugPrint("Captcha result: " + result.toString());
+          // debugPrint("Captcha result code: " + code + ", result: " + jsonResult.toString());
+          await validateCaptchaResult(result
+              .map((key, value) => MapEntry(key.toString(), value.toString())));
+        } else {
+          // 终端用户完成验证失败，自动重试
+          debugPrint("Captcha result code: " + code);
+        }
+      }, onError: (Map<String, dynamic> message) async {
+        debugPrint("Captcha error: " + message.toString());
+        String code = message["code"];
 
-          // TO-DO
-          // 处理验证中返回的错误
-          if (Platform.isAndroid) { // Android 平台
-            if (code == "-1") {
-              // Gt3RegisterData 参数不合法
-            }
-            else if (code == "201") {
-              // 网络无法访问
-            }
-            else if (code == "202") {
-              // Json 解析错误
-            }
-            else if (code == "204") {
-              // WebView 加载超时，请检查是否混淆极验 SDK
-            }
-            else if (code == "204_1") {
-              // WebView 加载前端页面错误，请查看日志
-            }
-            else if (code == "204_2") {
-              // WebView 加载 SSLError
-            }
-            else if (code == "206") {
-              // gettype 接口错误或返回为 null
-            }
-            else if (code == "207") {
-              // getphp 接口错误或返回为 null
-            }
-            else if (code == "208") {
-              // ajax 接口错误或返回为 null
-            }
-            else {
-              // 更多错误码参考开发文档
-              // https://docs.geetest.com/sensebot/apirefer/errorcode/android
-            }
+        // TO-DO
+        // 处理验证中返回的错误
+        if (Platform.isAndroid) {
+          // Android 平台
+          if (code == "-1") {
+            // Gt3RegisterData 参数不合法
+          } else if (code == "201") {
+            // 网络无法访问
+          } else if (code == "202") {
+            // Json 解析错误
+          } else if (code == "204") {
+            // WebView 加载超时，请检查是否混淆极验 SDK
+          } else if (code == "204_1") {
+            // WebView 加载前端页面错误，请查看日志
+          } else if (code == "204_2") {
+            // WebView 加载 SSLError
+          } else if (code == "206") {
+            // gettype 接口错误或返回为 null
+          } else if (code == "207") {
+            // getphp 接口错误或返回为 null
+          } else if (code == "208") {
+            // ajax 接口错误或返回为 null
+          } else {
+            // 更多错误码参考开发文档
+            // https://docs.geetest.com/sensebot/apirefer/errorcode/android
           }
+        }
 
-          if (Platform.isIOS) { // iOS 平台
-            if (code == "-1009") {
-              // 网络无法访问
-            }
-            else if (code == "-1004") {
-              // 无法查找到 HOST
-            }
-            else if (code == "-1002") {
-              // 非法的 URL
-            }
-            else if (code == "-1001") {
-              // 网络超时
-            }
-            else if (code == "-999") {
-              // 请求被意外中断, 一般由用户进行取消操作导致
-            }
-            else if (code == "-21") {
-              // 使用了重复的 challenge
-              // 检查获取 challenge 是否进行了缓存
-            }
-            else if (code == "-20") {
-              // 尝试过多, 重新引导用户触发验证即可
-            }
-            else if (code == "-10") {
-              // 预判断时被封禁, 不会再进行图形验证
-            }
-            else if (code == "-1") {
-              // Gt3RegisterData 参数不合法
-            }
-            else {
-              // 更多错误码参考开发文档
-              // https://docs.geetest.com/sensebot/apirefer/errorcode/ios
-            }
+        if (Platform.isIOS) {
+          // iOS 平台
+          if (code == "-1009") {
+            // 网络无法访问
+          } else if (code == "-1004") {
+            // 无法查找到 HOST
+          } else if (code == "-1002") {
+            // 非法的 URL
+          } else if (code == "-1001") {
+            // 网络超时
+          } else if (code == "-999") {
+            // 请求被意外中断, 一般由用户进行取消操作导致
+          } else if (code == "-21") {
+            // 使用了重复的 challenge
+            // 检查获取 challenge 是否进行了缓存
+          } else if (code == "-20") {
+            // 尝试过多, 重新引导用户触发验证即可
+          } else if (code == "-10") {
+            // 预判断时被封禁, 不会再进行图形验证
+          } else if (code == "-1") {
+            // Gt3RegisterData 参数不合法
+          } else {
+            // 更多错误码参考开发文档
+            // https://docs.geetest.com/sensebot/apirefer/errorcode/ios
           }
-
-        });
-    }
-    catch (e) {
+        }
+      });
+    } catch (e) {
       debugPrint("Event handler exception " + e.toString());
     }
 
@@ -166,18 +144,21 @@ class _MyAppState extends State<MyApp> {
     debugPrint("Start captcha. Current version: " + _platformVersion);
     // 添加时间戳，避免缓存
     // 如果 challenge 缓存，重复使用，会收到 -21 等错误
-    String api1 = "https://www.geetest.com/demo/gt/register-test?t=" + DateTime.now().toString();
+    String api1 = "https://www.geetest.com/demo/gt/register-test?t=" +
+        DateTime.now().toString();
     try {
       final response = await http.get(Uri.parse(api1));
       if (response.statusCode == 200) {
-        var jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+        var jsonResponse =
+            convert.jsonDecode(response.body) as Map<String, dynamic>;
         Gt3RegisterData registerData = Gt3RegisterData(
             gt: jsonResponse["gt"],
             challenge: jsonResponse["challenge"],
             success: jsonResponse["success"] == 1);
         captcha.startCaptcha(registerData);
       } else {
-        debugPrint(api1 + " response status: " + response.statusCode.toString());
+        debugPrint(
+            api1 + " response status: " + response.statusCode.toString());
       }
     } on SocketException {
       // 未联网时无法弹出验证窗口，在此处理无网络时的逻辑
@@ -193,22 +174,22 @@ class _MyAppState extends State<MyApp> {
   Future<dynamic> validateCaptchaResult(Map<String, String> result) async {
     String api2 = "https://www.geetest.com/demo/gt/validate-test";
     try {
-      final response = await http.post(
-          Uri.parse(api2),
-          headers: {"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"},
-          body: result
-      );
+      final response = await http.post(Uri.parse(api2),
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+          },
+          body: result);
       if (response.statusCode == 200) {
-        var jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+        var jsonResponse =
+            convert.jsonDecode(response.body) as Map<String, dynamic>;
         if (jsonResponse["status"].toString() == "success") {
           debugPrint("Validate success. Response: " + response.body);
-        }
-        else {
+        } else {
           debugPrint("Validate failure. Response: " + response.body);
         }
-      }
-      else {
-        debugPrint(api2 + " response status: " + response.statusCode.toString());
+      } else {
+        debugPrint(
+            api2 + " response status: " + response.statusCode.toString());
       }
     } on SocketException {
       // 未联网时无法完成二次验证，在此处理无网络时的逻辑
@@ -225,22 +206,21 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Center(
           child: TextButton(
-                  style: ButtonStyle(
-                    foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                    overlayColor: MaterialStateProperty.resolveWith<Color?>(
-                      (Set<MaterialState> states) {
-                        if (states.contains(MaterialState.hovered))
-                          return Colors.blue.withOpacity(0.04);
-                        if (states.contains(MaterialState.focused) ||
-                            states.contains(MaterialState.pressed))
-                          return Colors.blue.withOpacity(0.12);
-                        return null; // Defer to the widget's default.
-                      },
-                    ),
-                  ),
-                  onPressed: startCaptcha,
-                  child: Text('点击验证')
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.hovered))
+                      return Colors.blue.withOpacity(0.04);
+                    if (states.contains(MaterialState.focused) ||
+                        states.contains(MaterialState.pressed))
+                      return Colors.blue.withOpacity(0.12);
+                    return null; // Defer to the widget's default.
+                  },
                 ),
+              ),
+              onPressed: startCaptcha,
+              child: Text('点击验证')),
         ),
       ),
     );
--- lib/gt3_flutter_plugin.dart
+++ lib/gt3_flutter_plugin.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
@@ -10,7 +9,7 @@ class Gt3FlutterPlugin {
   static const String flutterLog = "| Geetest | Flutter | ";
   static const MethodChannel _channel = MethodChannel('gt3_flutter_plugin');
 
-  static String get version  {
+  static String get version {
     return "0.0.2";
   }
 
@@ -49,8 +48,10 @@ class Gt3FlutterPlugin {
   void addEventHandler({
     /// 验证视图展示
     EventHandler? onShow,
+
     /// 用户关闭验证视图
     EventHandler? onClose,
+
     /// 验证完成，获得验证校验参数
     /// 结构如下:
     /// {"result": {"geetest_challenge": ..., "geetest_seccode": ..., "geetest_validate": ...},
@@ -59,6 +60,7 @@ class Gt3FlutterPlugin {
     /// code 为 "1" 则完成验证，需进一步进行二次校验
     /// code 为 "0" 则验证失败，自动进行重试
     EventHandler? onResult,
+
     /// 错误回调
     /// 结构如下：
     /// {"description": ...},
@@ -70,29 +72,28 @@ class Gt3FlutterPlugin {
   }) {
     print(flutterLog + "addEventHandler:");
 
-    _onShow     = onShow;
-    _onClose    = onClose;
-    _onResult   = onResult;
-    _onError    = onError;
+    _onShow = onShow;
+    _onClose = onClose;
+    _onResult = onResult;
+    _onError = onError;
     _channel.setMethodCallHandler(_handler);
   }
 
   /// 原生回调
   Future<dynamic> _handler(MethodCall methodCall) async {
-
     switch (methodCall.method) {
       case "onShow":
-      print(flutterLog + "onShow:" + _onShow.toString());
-          return _onShow!(methodCall.arguments.cast<String, dynamic>());
+        print(flutterLog + "onShow:" + _onShow.toString());
+        return _onShow!(methodCall.arguments.cast<String, dynamic>());
       case "onClose":
-      print(flutterLog + "onClose:" + _onClose.toString());
-          return _onClose!(methodCall.arguments.cast<String, dynamic>());
+        print(flutterLog + "onClose:" + _onClose.toString());
+        return _onClose!(methodCall.arguments.cast<String, dynamic>());
       case "onResult":
-      print(flutterLog + "onResult:" + _onResult.toString());
-          return _onResult!(methodCall.arguments.cast<String, dynamic>());
+        print(flutterLog + "onResult:" + _onResult.toString());
+        return _onResult!(methodCall.arguments.cast<String, dynamic>());
       case "onError":
-      print(flutterLog + "onError:" + _onError.toString());
-          return _onError!(methodCall.arguments.cast<String, dynamic>());
+        print(flutterLog + "onError:" + _onError.toString());
+        return _onError!(methodCall.arguments.cast<String, dynamic>());
       default:
         throw UnsupportedError(flutterLog + "Unrecognized Event");
     }
@@ -102,25 +103,24 @@ class Gt3FlutterPlugin {
 class Gt3RegisterData {
   /// 验证 ID
   final String? gt;
+
   /// 验证 流水号
   final String? challenge;
+
   /// 是否进入宕机模式
   final bool? success;
 
-  const Gt3RegisterData({
-    @required this.gt,
-    @required this.challenge,
-    @required this.success
-  })
-  : assert(gt != null),
-    assert(challenge != null),
-    assert(success != null);
+  const Gt3RegisterData(
+      {@required this.gt, @required this.challenge, @required this.success})
+      : assert(gt != null),
+        assert(challenge != null),
+        assert(success != null);
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'gt': gt,
       'challenge': challenge,
       'success': success
-      }..removeWhere((key, value) => value == null);
+    }..removeWhere((key, value) => value == null);
   }
 }
